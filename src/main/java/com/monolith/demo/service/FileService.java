0337398cc2245ddc350900d6c1408eba3550c647
Leonvandervleuten <63288282+Leonvandervleuten@users.noreply.github.com>
2023-05-31 20:17:22 +0200

    rest call to 3th service

--- src/main/java/com/monolith/demo/service/FileService.java
+++ src/main/java/com/monolith/demo/service/FileService.java
@@ -5,10 +5,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -18,21 +14,8 @@ public class FileService {
 
   String url = "http://localhost:8082/file";
 
-  public List<FileEntity> getFile() throws IOException {
-    return makeMultipleFileObjectAndReturn();
-  }
-
-  private List<FileEntity> makeMultipleFileObjectAndReturn() throws IOException {
-
-    List<FileEntity> fileList;
-    fileList = getFileFromThirdService();
-
-    for (int i = 0; i < fileList.size(); i++) {
-      FileEntity fe = new FileEntity();
-      fe.setFileEntity(getFileFromThirdService().get(i).getFileEntity());
-      fileList.add(fe);
-    }
-    return fileList;
+  public List<FileEntity> getFile() {
+    return getFileFromThirdService();
   }
 
   private List<FileEntity> getFileFromThirdService() {
