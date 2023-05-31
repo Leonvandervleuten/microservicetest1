4a47da87b61d8271e3563341b98478671ef11d28
Leonvandervleuten <63288282+Leonvandervleuten@users.noreply.github.com>
2023-05-31 16:38:53 +0200

    Resttemplate geimplementeerd om data van 1e microservice op te halen

--- src/main/java/com/monolith/demo/service/FileService.java
+++ src/main/java/com/monolith/demo/service/FileService.java
@@ -1,37 +1,27 @@
 package com.monolith.demo.service;
 
 import com.monolith.demo.domain.FileEntity;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.util.ArrayList;
 import java.util.List;
 
 @Service
 public class FileService {
 
-  public List<FileEntity> getFile() throws IOException {
-    return makeMultipleFileObjectAndReturn();
+  RestTemplate restTemplate = new RestTemplate();
+
+  String url = "http://localhost:8081/file";
+
+  public List<FileEntity> getFile() {
+    return null;
   }
 
-  private List<FileEntity> makeMultipleFileObjectAndReturn() throws IOException {
-
-    List<FileEntity> fileList = new ArrayList<>();
-
-    for (int i = 0; i < 5; i++) {
-      FileEntity fe = new FileEntity();
-      fe.setFileEntity(readFile().getFileEntity());
-      fileList.add(fe);
-    }
-    return fileList;
-  }
-
-  private FileEntity readFile() throws IOException {
-    FileEntity fileEntity = new FileEntity();
-    fileEntity.setFileEntity(Files.readAllBytes(Paths.get("src/main/resources/util/Free_Test_Data_10.5MB_PDF.pdf")));
-    return fileEntity;
+  private List<FileEntity> getFileFromSecondService() {
+    ResponseEntity<FileEntity[]> response = restTemplate.getForEntity(url, FileEntity[].class);
+    return (List<FileEntity>) response;
   }
 
 }
